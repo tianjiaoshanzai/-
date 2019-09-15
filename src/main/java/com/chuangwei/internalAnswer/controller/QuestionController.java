a660431fa6f95bfe0732b588c6b3132d6d2a0a29
tianjiaoshanzai <tianjiaoshanzai@gmail.com>
2019-09-15 23:46:28 +0800

    增加了发帖，html以及敏感词过滤

---
+++ src/main/java/com/chuangwei/internalAnswer/controller/QuestionController.java
@@ -0,0 +1,77 @@
+package com.chuangwei.internalAnswer.controller;
+
+import com.chuangwei.internalAnswer.model.HostHolder;
+import com.chuangwei.internalAnswer.model.Question;
+import com.chuangwei.internalAnswer.model.ViewObject;
+import com.chuangwei.internalAnswer.service.QuestionService;
+import com.chuangwei.internalAnswer.service.UserService;
+import com.chuangwei.internalAnswer.util.InternalAnswerUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+@Controller
+public class QuestionController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @Autowired
+    QuestionService questionService;
+
+    //@Autowired
+    //CommentService commentService;
+
+    @Autowired
+    HostHolder hostHolder;//拦截器
+
+    @Autowired
+    UserService userService;
+
+    /*@RequestMapping(value = "/question/{qid}", method = {RequestMethod.GET})
+    public String questionDetail(Model model, @PathVariable("qid") int qid) {
+        Question question = questionService.getById(qid);
+        model.addAttribute("question", question);
+        List<Comment> commentList = commentService.getCommentsByEntity(qid, EntityType.ENTITY_QUESTION);
+        List<ViewObject> vos = new ArrayList<>();
+        for (Comment comment : commentList) {
+            ViewObject vo = new ViewObject();
+            vo.set("comment", comment);
+            vo.set("user", userService.getUser(comment.getUserId()));
+            vos.add(vo);
+        }
+        model.addAttribute("comments", vos);
+
+        return "detail";
+    }*/
+
+    @RequestMapping(value = "/question/add", method = {RequestMethod.POST})
+    @ResponseBody
+    public String addQuestion(@RequestParam("title") String title, @RequestParam("content") String content) {
+        try {
+            Question question = new Question();
+            question.setContent(content);
+            question.setCreatedDate(new Date());
+            question.setTitle(title);
+            if (hostHolder.getUser() == null) {
+                question.setUserId(InternalAnswerUtil.ANONYMOUS_USERID);//若得不到用户，给一个匿名用户
+                // return InternalAnswerUtil.getJSONString(999);
+            } else {
+                question.setUserId(hostHolder.getUser().getId());
+            }
+            if (questionService.addQuestion(question) > 0) {
+                return InternalAnswerUtil.getJSONString(0);
+            }
+        } catch (Exception e) {
+            logger.error("增加题目失败" + e.getMessage());
+        }
+        return InternalAnswerUtil.getJSONString(1, "失败");
+    }
+
+}
